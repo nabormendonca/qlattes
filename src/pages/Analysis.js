6b9596b3158d1e758f041b635b27b27ecd8077a9
Rebeca Stroh <rebecapstroh@gmail.com>
2023-04-03 00:27:29 -0300

    add icons

--- src/pages/Analysis.js
+++ src/pages/Analysis.js
@@ -7,6 +7,8 @@ import ScoreGraphicView from '../components/ScoreGraphicView';
 import ScoreTableView from '../components/ScoreTableView';
 import TopView from '../components/TopView';
 
+import { FaTrashAlt, FaUserAlt, FaChartBar, FaRegCalendarCheck } from 'react-icons/fa';
+
 // const authors = ["Stevens Kastrup Rehen", "Carlos José Pereira de Lucena", "Joel José Puga Coelho Rodrigues"];
 
 async function updateLattesData() {
@@ -160,42 +162,53 @@ function Analysis() {
   return (
     <div class="content">
       <form id="form-filters">
-        <select id="author-select" value={author} onChange={e => handleAuthorSelector(e.target.value)}>
-          <option value="" disabled="true" selected="true" hidden="true">Selecione um CV</option>
-          {authors.map(op => <option value={op.link}>{op.name}</option>)}
-        </select>
+        <div class="select-icon">
+          <FaUserAlt/> 
+          <select id="author-select" value={author} onChange={e => handleAuthorSelector(e.target.value)}>
+            <option value="" disabled="true" selected="true" hidden="true">Selecione um CV</option>
+            {authors.map(op => <option value={op.link}>{op.name}</option>)}
+          </select>
+        </div>
         { author != "" ? <>
           <button id="clear-data-button" title="Remover dados do CV" onClick={() => setAuthor("")}>
-            DELETE
+            <FaTrashAlt />
           </button>
-          <select id="view-type-select" value={viewType} onChange={e => handleViewTypeSelector(e.target.value)}>
-            <option value="" disabled="true" selected="true" hidden="true">Selecione uma visualização</option>
-            <optgroup label="Classificação">
-              <option value="qualisTableView">Tabela de classificação Qualis</option>
-              <option value="qualisGraphicView">Gráfico de classificação Qualis</option>
-            </optgroup>
-            <optgroup label="Pontuação">
-              <option value="scoreTableView">Tabela de pontuação Qualis</option>
-              <option value="scoreGraphicView">Gráfico de pontuação Qualis</option>
-            </optgroup>
-            <optgroup label="Publicações">
-              <option value="top5View">5 melhores artigos</option>
-              <option value="top10View">10 melhores artigos</option>
-            </optgroup>
-          </select>
-          <p id="total-pubs-div">{totalPubs} artigos em periódicos entre {initYear} e {endYear}</p>
+          <div class="select-icon">
+            <FaChartBar/>
+            <select id="view-type-select" value={viewType} onChange={e => handleViewTypeSelector(e.target.value)}>
+              <option value="" disabled="true" selected="true" hidden="true"> Selecione uma visualização</option>
+              <optgroup label="Classificação">
+                <option value="qualisTableView">Tabela de classificação Qualis</option>
+                <option value="qualisGraphicView">Gráfico de classificação Qualis</option>
+              </optgroup>
+              <optgroup label="Pontuação">
+                <option value="scoreTableView">Tabela de pontuação Qualis</option>
+                <option value="scoreGraphicView">Gráfico de pontuação Qualis</option>
+              </optgroup>
+              <optgroup label="Publicações">
+                <option value="top5View">5 melhores artigos</option>
+                <option value="top10View">10 melhores artigos</option>
+              </optgroup>
+            </select>
+          </div>
           <input id="init-year-input" class="year-input" type="number" min={initYear} max={endYear} value={initYearInput} required="required" onChange={e => setInitYearInput(e.target.value)}/>
           <p> a </p>
           <input id="end-year-input" class="year-input" type="number" min={initYear} max={endYear} value={endYearInput} required="required" onChange={e => setEndYearInput(e.target.value)}/>
-          <select id="period-select" onChange={e => handleSelectedPeriod(e.target.value)}>
-            <option value="last5">Últimos 5 anos</option>
-            <option value="last10">Últimos 10 anos</option>
-            <option value="all" selected="true">Todo o período do CV</option>
-          </select>
-          <input id="showStatistics" type="checkbox" value={showStatistics} onChange={e => setShowStatistics(e.target.value)}/>
-          <label for="showStatistics">Exibir estatísticas</label>
+          <div class="select-icon">
+            <FaRegCalendarCheck/>
+            <select id="period-select" onChange={e => handleSelectedPeriod(e.target.value)}>
+              <option value="last5">Últimos 5 anos</option>
+              <option value="last10">Últimos 10 anos</option>
+              <option value="all" selected="true"> Todo o período do CV</option>
+            </select>
+          </div>
+          <div>
+            <input id="showStatistics" type="checkbox" value={showStatistics} onChange={e => setShowStatistics(e.target.value)}/>
+            <label for="showStatistics">Exibir estatísticas</label>
+          </div>
         </> : null}
       </form>
+      { author != "" && <p id="total-pubs-div">{totalPubs} artigos em periódicos entre {initYear} e {endYear}</p> }
       <div class="table-wrapper">
         {viewType == "qualisTableView" ? <QualisTableView init={initYearInput} end={endYearInput} stats={stats} showStatistics={showStatistics}/> : null}
         {viewType == "qualisGraphicView" ? <QualisGraphicView init={initYearInput} end={endYearInput} stats={stats}/> : null}
