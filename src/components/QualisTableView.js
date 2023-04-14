6b1fda7afa707271a9dbaab5e1f25e8036533efb
Rebeca Stroh <rebecapstroh@gmail.com>
2023-04-13 23:47:57 -0300

    reuses functions on qualis table

--- src/components/QualisTableView.js
+++ src/components/QualisTableView.js
@@ -1,16 +1,13 @@
 import '../App.css';
-import { linearRegression, updateTotalStats } from '../Utils/utils.js';
+import {
+  qualisScores,
+  getTableClass,
+  addStatisticFromTotal,
+  updateTotalStats
+} from '../Utils/utils.js';
 
 function QualisTableView({init, end, stats, showStatistics}) {
-  const firstCol = {
-    label: 'Ano',
-    type: 'year',
-  };
-  const dataCols = {
-    keys: ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C', 'N'],
-    labels: ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C', 'N'],
-    type: 'data',
-  };
+  const dataCols = Object.keys(qualisScores);
   const totalCols = {
     keys: ['totA', 'totB', 'totABC'],
     labels: ['#A', '#B', 'Total'],
@@ -19,13 +16,13 @@ function QualisTableView({init, end, stats, showStatistics}) {
 
   // reset data counts
   let dataCounts = {};
-  for (const key of dataCols.keys) {
+  for (const key of dataCols) {
     dataCounts[key] = 0;
   }
 
   // reset total counts
   let totalCounts = {};
-  for (const key of dataCols.keys) {
+  for (const key of dataCols) {
     totalCounts[key] = 0;
   }
   for (const key of totalCols.keys) {
@@ -56,7 +53,7 @@ function QualisTableView({init, end, stats, showStatistics}) {
       }
       
       // create cells with data cols
-      for (const key of dataCols.keys) {
+      for (const key of dataCols) {
         newRow.push(<td type='data'>{stats[key][currYear]}</td>);
         const keyChar = key.slice(0, 1);
 
@@ -89,64 +86,21 @@ function QualisTableView({init, end, stats, showStatistics}) {
     }
   }
 
-  // add new foot row cell to table footer row
-  let meanRow = []
-  let medianRow = []
-  let trendRow = []
-  let bestYearRow = []
-  for (const col of dataCols.keys) {
-    meanRow.push(<th type="data"></th>);
-    medianRow.push(<th type="data"></th>);
-    trendRow.push(<th type="data"></th>);
-    bestYearRow.push(<th type="data"></th>);
-  }
-
-  // Best Year
-  for (const col of totalCols.keys) {
-    meanRow.push(<th type='total'>{totalStats[col].countList
-      .mean()
-      .toFixed(2)
-      .replace('.', ',')}</th>);
-    medianRow.push(<th type='total'>{totalStats[col].countList
-      .median()
-      .toFixed(2)
-      .replace('.', ',')}</th>);
-    trendRow.push(<th type='total'>{linearRegression(
-      totalStats[col].yearList,
-      totalStats[col].countList
-    )
-      .slope.toFixed(2)
-      .replace('.', ',')}</th>);
-    bestYearRow.push(<th type='total'>{totalStats[col].best.year > 0 ? totalStats[col].best.year : ''}</th>);
-  }
-
-  const rowHeight = 20.5;
-  let tableClass = "styled-table";
-  const maxHeight = window.innerHeight*0.50;
-  if (rowHeight*rows.length > maxHeight) {
-    tableClass += ' has-scroll';
-  }
-
   return (
-    <table class={tableClass} id="qualis-table">
+    <table class={getTableClass(rows.length)} id="qualis-table">
       <thead><tr>
-        <th type={firstCol.type}>{firstCol.label}</th>
-        {dataCols.labels.map(dataElem => <th type={dataCols.type}>{dataElem}</th>)}
-        {totalCols.labels.map(totalElem => <th type={totalCols.type}>{totalElem}</th>)}
+        <th type="year">Ano</th>
+        {dataCols.map(dataElem => <th type="data">{dataElem}</th>)}
+        {totalCols.labels.map(totalElem => <th type="total">{totalElem}</th>)}
       </tr></thead>
       <tbody>{rows}</tbody>
       <tfoot>
         <tr tag="total">
           <th type="year">Total</th>
-          {dataCols.keys.map(key => <th type={dataCols.type}>{totalCounts[key]}</th>)}
-          {totalCols.keys.map(key => <th type={totalCols.type}>{totalCounts[key]}</th>)}
+          {dataCols.map(key => <th type="data">{totalCounts[key]}</th>)}
+          {totalCols.keys.map(key => <th type="total">{totalCounts[key]}</th>)}
         </tr>
-        {showStatistics ? (<>
-          <tr> <th type="label">Média</th> {meanRow} </tr>
-          <tr> <th type="label">Mediana</th> {medianRow} </tr>
-          <tr> <th type="label">Tendência</th> {trendRow} </tr>
-          <tr> <th type="label">Melhor ano</th> {bestYearRow} </tr>
-        </>) : null}
+        {showStatistics ? addStatisticFromTotal(totalCols.keys, totalStats): null}
       </tfoot>
     </table>
   );
